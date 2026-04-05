440a86e55d5ace659dcc9c4eed070417794b081c
ayshath-rushda <rushdav125@gmail.com>
2026-04-05 22:54:27 +0530

    project init

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import authRoute from "./routes/auth.js";
+import hotelsRoute from "./routes/hotels.js";
+import usersRoute from "./routes/users.js";
+import roomsRoute from "./routes/rooms.js";
+
+const app = express();
+dotenv.config()
+
+
+const handleError = (err) => {
+  console.error("MongoDB error:", err);
+};
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("connected to mongodb");
+  } catch (error) {
+    handleError(error);
+  }
+};
+mongoose.connection.on("disconnected", ()=>{
+    console.log("mongodb is disconnected");
+    
+})
+
+
+//middlewares
+app.use(express.json())
+
+app.use("/api/auth",authRoute);
+app.use("/api/users",usersRoute);
+app.use("/api/hotels" , hotelsRoute);
+app.use("/api/rooms",roomsRoute);
+
+
+
+
+app.listen(9000 ,()=>{
+    connect()
+    console.log("backend connected")
+});
---
+++ routes/hotels.js
@@ -0,0 +1,85 @@
+import express from "express";
+import Hotel from "../models/Hotel.js"; 
+
+const router = express.Router();
+
+//create hotel
+router.post("/", async (req,res)=>{
+   
+    const newHotel = new Hotel(req.body)
+   
+   
+    try{
+        const savedHotel=await newHotel.save()
+        res.status(200).json(savedHotel)
+
+    }catch(err){
+        res.status(500).json(err)
+    }    
+
+
+});
+
+//update hotel
+router.put("/:id", async (req,res)=>{
+   
+   
+    try{
+        const updatedHotel=await Hotel.findByIdAndUpdate(req.params.id,
+            {$set:req.body},
+            {returnDocument:"after",
+                runValidators:true
+            }
+        );
+        res.status(200).json(updatedHotel)
+
+    }catch(err){
+        res.status(500).json(err)
+    }    
+
+
+});
+
+//delete
+router.delete("/:id", async (req,res)=>{
+    try{
+        await Hotel.findByIdAndDelete(
+            req.params.id,
+            {$set:req.body},
+            {new:true}
+        );
+        res.status(200).json("hotel has been deleted")
+
+    }catch(err){
+        res.status(500).json(err)
+    }    
+});
+
+//get
+router.get("/:id", async (req,res)=>{
+    try{
+        const hotel=await Hotel.findById(req.params.id);
+        res.status(200).json(hotel);
+
+    }catch(err){
+        res.status(500).json(err)
+    }    
+});
+
+
+//get all
+router.get("/", async (req,res)=>{
+   
+   
+    try{
+        const hotels=await Hotel.find(req.params.id);
+        res.status(200).json(hotels)
+
+    }catch(err){
+        res.status(500).json(err)
+    }    
+
+
+});
+
+export default router
