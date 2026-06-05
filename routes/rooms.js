bef95e70d8de0522ed365f5fd9f58a96112181f3
ayshath-rushda <rushdav125@gmail.com>
2026-06-05 17:46:45 +0530

    Implement room CRUD operations

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,5 +1,23 @@
 import express from "express";
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controller/room.js";
+import { verifyAdmin } from "../util/verifyToken.js";
 
 const router = express.Router();
+
+//create Room
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+//update Room
+router.put("/:id", verifyAdmin, updateRoom);
+
+//delete
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+//get
+router.get("/:id", getRoom);
+
+
+//get all
+router.get("/", getRooms);
 
 export default router
